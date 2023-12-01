3178efd2fc7e85b45c9f53706ceba460b937f779
Team7034 <wlhsfrc@gmail.com>
2023-11-30 19:58:55 -0800

    intake command + PID (NOt finished - encoder conversion facgtor)

--- src/main/java/frc/robot/commands/SpinIntake.java
+++ src/main/java/frc/robot/commands/SpinIntake.java
@@ -9,14 +9,26 @@ import static frc.robot.Constants.Intake.*;
 
 public class SpinIntake extends CommandBase {
     IntakeSubsystem intake;
+    boolean IntakeActive;
 
-    public SpinIntake(IntakeSubsystem intake) {
+    public SpinIntake(IntakeSubsystem intake, boolean ActiveIntake) {
         this.intake = intake;
+        IntakeActive = ActiveIntake;
+
+                    }
     }
+
 
     @Override
     public void execute() {
         //intake.setLiftSpeed(SmartDashboard.getNumber("Lift Speed", 0));
         //intake.setSpinSpeed(SmartDashboard.getNumber("Spin Speed", 0));
+
+        if (IntakeActive){
+            intake.setGoalPosition(intake.activePosition);
+
+        } else {
+            intake.setGoalPosition(intake.homePosition);
+        }
     }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import static frc.robot.Constants.Intake.*;
@@ -11,9 +13,18 @@ public class IntakeSubsystem extends SubsystemBase {
     CANSparkMax liftMotor;
     CANSparkMax spinMotor;
 
+    RelativeEncoder liftMotorEncoder;
+    PIDController liftpid;
+    public double homePosition = 0;
+    public double activePosition = 90;
+    double goalPosition = homePosition;
     public IntakeSubsystem() {
-        //liftMotor = new CANSparkMax(10, CANSparkMaxLowLevel.MotorType.kBrushless);
-        //spinMotor = new CANSparkMax(20, CANSparkMaxLowLevel.MotorType.kBrushless);
+        liftMotor = new CANSparkMax(10, CANSparkMaxLowLevel.MotorType.kBrushless);
+        spinMotor = new CANSparkMax(20, CANSparkMaxLowLevel.MotorType.kBrushless);
+        //TODO set encoder conversino factor
+        liftMotorEncoder = liftMotor.getEncoder();
+        liftpid = new PIDController(0,0,0);
+        liftMotorEncoder.setPosition(homePosition);
     }
 
     public void setLiftSpeed(double speed) {
@@ -22,5 +33,12 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public void setSpinSpeed(double speed) {
         spinMotor.set(speed);
+    }
+
+    public void periodic(){
+        setLiftSpeed(liftpid.calculate(liftMotorEncoder.getPosition(), goalPosition));
+    }
+    public void setGoalPosition(double goalPosition){
+        this.goalPosition = goalPosition;
     }
 }
